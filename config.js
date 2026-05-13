c80a14aaab55c7def7e84d6182e1f177e9a27fcb
muratanel <muratanel7@gmail.com>
2026-05-13 22:43:56 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Файл глобальных настроек (config.js)
 
 // 1. Вставьте сюда ваш актуальный ENCODED текст для Google Apps Script
-const APPS_SCRIPT_URL = "https://script.google.com/macros/library/d/1gSzWIKOWzrMlzb-g9OlU1_xr3mOB_uv8in0pCotqjrLB4zVXGtxIbFy8/76"; 
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzeaUQamuGOih-r--nSvEEkidNREnllZdbL27oc_c0DTGEhHZJe4-L22Xw2-Gd9HC9K/exec"; 
 
 // 2. Ссылка на вебхук Make.com
 // const _0x5bc2 = "aHR0cHM6Ly9ob29rLmV1MS5tYWtlLmNvbS85dWQyeWV0dHIzbDZrdG81bmx0cXR0am9jZmI2cDZvZw==";
