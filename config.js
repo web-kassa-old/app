2a4e055b09ffc83d1f3e7fd411ae32f8364d401e
muratanel <muratanel7@gmail.com>
2026-06-29 12:49:59 +0500

    Update SaaS router URL in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Файл глобальных настроек (config.js)
 
 // 1. Ссылка на новый SaaS-роутер (POS Master Control)
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyOp6fzexVQwUHVNmL50hF62pz20TW9nNoTL1SKyBEVGb095SVh_h6kNdyOIaMdroyW/exec"; 
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwqoibThI304igVBBhs3dN_ethD3Hq0WPVPKcfLuOJEKirZs8EDu0ItGP1bxmCMUiQbKA/exec"; 
 
 // 2. Уникальный ключ клиента (Tenant ID / API Key)
 const CLIENT_API_KEY = "TEST_STORE_001";
