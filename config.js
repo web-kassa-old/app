5943720f09bfb0258f2fe9b987f316a18c3f3ccc
muratanel <muratanel7@gmail.com>
2026-07-03 17:30:46 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 // Файл глобальных настроек (config.js)
 
-const APP_VERSION = "5.5.11"; // Меняйте версию только здесь
+const APP_VERSION = "5.5.1"; // Меняйте версию только здесь
+
+// Глобальные настройки приложения
+const CONFIG = {
+    // Вставьте сюда вашу актуальную ссылку веб-приложения из Google Apps Script
+    API_URL: "https://script.google.com/macros/s/AKfycbyOp6fzexVQwUHVNmL50hF62pz20TW9nNoTL1SKyBEVGb095SVh_h6kNdyOIaMdroyW/exec"
+};
 
 // 1. Ссылки на роутер и шлюз (единые для всех арендаторов)
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyOp6fzexVQwUHVNmL50hF62pz20TW9nNoTL1SKyBEVGb095SVh_h6kNdyOIaMdroyW/exec"; 
