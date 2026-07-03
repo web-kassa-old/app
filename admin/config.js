ada72201bd6b04e53b4b6b66c42b6795a0b63de8
muratanel <muratanel7@gmail.com>
2026-07-03 19:30:04 +0500

    Add files via upload

--- admin/config.js
+++ admin/config.js
@@ -1,5 +1,7 @@
-// Глобальные настройки приложения //
+// Глобальные настройки приложения
 const CONFIG = {
+    VERSION: "1.0.1", // Меняйте эту цифру при каждом обновлении интерфейса
+    
     // Вставьте сюда вашу актуальную ссылку веб-приложения из Google Apps Script
     API_URL: "https://script.google.com/macros/s/AKfycbyOp6fzexVQwUHVNmL50hF62pz20TW9nNoTL1SKyBEVGb095SVh_h6kNdyOIaMdroyW/exec"
 };
