cf56bf2c88f7981900df98abfa2c698364267350
muratanel <muratanel7@gmail.com>
2026-07-04 22:54:34 +0500

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -1,6 +1,6 @@
 // Глобальные настройки приложения
 const CONFIG = {
-    VERSION: "1.0.15", // Меняйте эту цифру при каждом обновлении интерфейса
+    VERSION: "1.0.16", // Меняйте эту цифру при каждом обновлении интерфейса
     
     // Вставьте сюда вашу актуальную ссылку веб-приложения из Google Apps Script
     API_URL: "https://script.google.com/macros/s/AKfycbyOp6fzexVQwUHVNmL50hF62pz20TW9nNoTL1SKyBEVGb095SVh_h6kNdyOIaMdroyW/exec"
