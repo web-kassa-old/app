c676ff1e92334a2dde2b0f7f7eb4c6eee31df624
muratanel <muratanel7@gmail.com>
2026-07-08 11:23:29 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,14 +1,21 @@
 // Файл глобальных настроек (config.js)
 
-const APP_VERSION = "5.5.04"; // Меняйте версию только здесь
+const APP_VERSION = "5.5.03"; // Меняйте версию только здесь
 
 // 1. Ссылки на роутер и шлюз (единые для всех арендаторов)
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyOp6fzexVQwUHVNmL50hF62pz20TW9nNoTL1SKyBEVGb095SVh_h6kNdyOIaMdroyW/exec"; 
 const GATEWAY_URL = "https://script.google.com/macros/s/AKfycbw2C9Pf7IM063RUK7niLCKxlmobGvW4s78FoX-vXRLHe_n-nScJajt4DuljfoJoNVN1/exec";
-// === НОВАЯ КОНСТАНТА: Ключ авторизации Google ===
+
+// 2. Ключ авторизации Google
 const GOOGLE_CLIENT_ID = "1071015264018-s100se0ek42af15rm9vhq260b3lr7qer.apps.googleusercontent.com";
 
-// 2. Динамический перехват уникального ключа клиента (Tenant ID)
+// === 3. ДАННЫЕ ДЛЯ УСТАНОВЩИКА ===
+// Вставляй сюда полные ссылки на твои шаблоны
+const TEMPLATE_TRADE_URL = "https://docs.google.com/spreadsheets/d/1FVBka2zyM50hSGKE59kyhcZwolMzUq57nkOiwbchaTg/edit?gid=1354673204#gid=1354673204";
+const TEMPLATE_CONFIG_URL = "https://docs.google.com/spreadsheets/d/18J1wePHlRtqcmOA86fHxGwb38s70sJEmZafL2mvUR1I/edit?gid=1992649776#gid=1992649776";
+const ADMIN_EMAIL = "muratanel7@gmail.com"; 
+
+// 4. Динамический перехват уникального ключа клиента (Tenant ID)
 const urlParams = new URLSearchParams(window.location.search);
 const urlApiKey = urlParams.get('api_key');
 
@@ -21,10 +28,10 @@ if (urlApiKey) {
     window.history.replaceState({ path: cleanUrl }, '', cleanUrl);
 }
 
-// 3. Инициализация константы для работы приложения
+// 5. Инициализация константы для работы приложения
 const CLIENT_API_KEY = localStorage.getItem('CLIENT_API_KEY') || "TEST_STORE_001";
 
 // Проверка на отсутствие ключа
-if (!CLIENT_API_KEY) {
-    console.error("Ошибка аутентификации: Ключ арендатора не найден. Обратитесь к администратору.");
+if (!CLIENT_API_KEY || CLIENT_API_KEY === "TEST_STORE_001") {
+    console.warn("Внимание: Используется тестовый или пустой ключ арендатора. Обратитесь к администратору.");
 }
