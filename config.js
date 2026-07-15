c4c9e6eb2f8243c45b74047207ac02d81c3af4b3
muratanel <muratanel7@gmail.com>
2026-07-15 09:51:38 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Файл глобальных настроек (config.js)
 
-const APP_VERSION = "5.6.09"; // Меняйте версию только здесь
+const APP_VERSION = "5.5.45"; // Меняйте версию только здесь
 
 // 1. Ссылки на роутер и шлюз (единые для всех арендаторов)
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyOp6fzexVQwUHVNmL50hF62pz20TW9nNoTL1SKyBEVGb095SVh_h6kNdyOIaMdroyW/exec"; 
@@ -14,6 +14,13 @@ const GOOGLE_CLIENT_ID = "1071015264018-s100se0ek42af15rm9vhq260b3lr7qer.apps.go
 const TEMPLATE_TRADE_URL = "https://docs.google.com/spreadsheets/d/1FVBka2zyM50hSGKE59kyhcZwolMzUq57nkOiwbchaTg/edit?gid=1354673204#gid=1354673204";
 const TEMPLATE_CONFIG_URL = "https://docs.google.com/spreadsheets/d/18J1wePHlRtqcmOA86fHxGwb38s70sJEmZafL2mvUR1I/edit?gid=1992649776#gid=1992649776";
 const ADMIN_EMAIL = "muratanel7@gmail.com"; 
+
+// === 4. СПРАВОЧНЫЕ МАТЕРИАЛЫ ===
+// Ссылки на руководство пользователя POS
+const HELP_LINKS = {
+  ru: "https://dot-leaf-a35.notion.site/POS-39d1cf6d5f4f8038841fe8104985d573?source=copy_link",
+  kz: "https://dot-leaf-a35.notion.site/POS-39e1cf6d5f4f80aea32ee33e3f31bd03?source=copy_link"
+};
 
 // Динамически берем ключ из памяти телефона (он сохраняется туда после логина через Google)
 let CLIENT_API_KEY = localStorage.getItem('CLIENT_API_KEY') || "";
